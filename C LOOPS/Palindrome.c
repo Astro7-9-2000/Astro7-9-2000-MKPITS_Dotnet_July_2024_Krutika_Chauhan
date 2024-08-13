38b15dcb2cfeba124b50f575dfd09b127f1b3cd9
Krutika Chauhan <krutikachauhan07@gmail.com>
2024-08-13 11:49:49 +0530

    Removed C Loops folder

--- C LOOPS/Palindrome.c
+++
@@ -1,6 +0,0 @@
-/*38. Write a C program to check whether a number is a palindrome or not.
-Test Data :
-Input a number: 121
-Expected Output :
-121 is a palindrome number.*/
-
--- C LOOPS/ReverseString.c
+++
@@ -1,6 +0,0 @@
-/*57. Write a C program to print a string in reverse order.
-Test Data :
-Input a string to reverse : Welcome
-Expected Output :
-Reversed string is: emocleW
-*/
