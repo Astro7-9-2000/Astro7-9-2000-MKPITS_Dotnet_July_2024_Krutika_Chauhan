a77e68125cb93f7e41ce05cee9451ed1fe663d09
Krutika Chauhan <krutikachauhan07@gmail.com>
2024-08-13 11:19:30 +0530

    C Loops program

---
+++ C LOOPS/ReverseString.c
@@ -0,0 +1,6 @@
+/*57. Write a C program to print a string in reverse order.
+Test Data :
+Input a string to reverse : Welcome
+Expected Output :
+Reversed string is: emocleW
+*/
---
+++ C LOOPS/Sum&Avg.c
@@ -0,0 +1,10 @@
+/* Write a program in C to read 10 numbers from the keyboard and find their sum and average.
+Test Data :
+Input the 10 numbers :
+Number-1 :1
+...
+Number-10 :10
+Expected Output :
+The sum of 10 no is : 55
+The Average is : 5.500000 */
+
