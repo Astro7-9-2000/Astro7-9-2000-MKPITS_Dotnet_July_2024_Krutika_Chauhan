cbd8133395448684748dfe98667451f5ba670617
Krutika Chauhan <krutikachauhan07@gmail.com>
2024-09-05 23:46:16 +0530

    Committing a short CRUD operation using Windows Forms

---
+++ C# Assignments/CRUDDemoForm/CRUDDemoForm/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CRUDDemoForm
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtId = new System.Windows.Forms.RichTextBox();
+            this.TxtName = new System.Windows.Forms.RichTextBox();
+            this.TxtAge = new System.Windows.Forms.RichTextBox();
+            this.InsertBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelID.Location = new System.Drawing.Point(31, 48);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(33, 25);
+            this.labelID.TabIndex = 0;
+            this.labelID.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(171, 48);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(417, 34);
+            this.TxtId.TabIndex = 3;
+            this.TxtId.Text = "";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(171, 111);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(417, 34);
+            this.TxtName.TabIndex = 4;
+            this.TxtName.Text = "";
+            // 
+            // TxtAge
+            // 
+            this.TxtAge.Location = new System.Drawing.Point(171, 175);
+            this.TxtAge.Name = "TxtAge";
+            this.TxtAge.Size = new System.Drawing.Size(417, 34);
+            this.TxtAge.TabIndex = 5;
+            this.TxtAge.Text = "";
+            // 
+            // InsertBtn
+            // 
+            this.InsertBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.InsertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsertBtn.Location = new System.Drawing.Point(55, 263);
+            this.InsertBtn.Name = "InsertBtn";
+            this.InsertBtn.Size = new System.Drawing.Size(132, 46);
+            this.InsertBtn.TabIndex = 6;
+            this.InsertBtn.Text = "INSERT";
+            this.InsertBtn.UseVisualStyleBackColor = false;
+            this.InsertBtn.Click += new System.EventHandler(this.InsertBtn_Click);
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.UpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateBtn.Location = new System.Drawing.Point(256, 263);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(132, 46);
+            this.UpdateBtn.TabIndex = 5;
+            this.UpdateBtn.Text = "UPDATE";
+            this.UpdateBtn.UseVisualStyleBackColor = false;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.DeleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBtn.Location = new System.Drawing.Point(467, 263);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(132, 46);
+            this.DeleteBtn.TabIndex = 6;
+            this.DeleteBtn.Text = "DELETE";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.SearchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchBtn.Location = new System.Drawing.Point(675, 263);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(132, 46);
+            this.SearchBtn.TabIndex = 7;
+            this.SearchBtn.Text = "SEARCH";
+            this.SearchBtn.UseVisualStyleBackColor = false;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(171, 359);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowHeadersWidth = 51;
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(507, 236);
+            this.DataGridView.TabIndex = 8;
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(988, 632);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.InsertBtn);
+            this.Controls.Add(this.TxtAge);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelID);
+            this.Name = "DemoForm";
+            this.Text = "DemoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox TxtId;
+        private System.Windows.Forms.RichTextBox TxtName;
+        private System.Windows.Forms.RichTextBox TxtAge;
+        private System.Windows.Forms.Button InsertBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.DataGridView DataGridView;
+    }
+}
+
---
+++ C# Assignments/CRUDDemoForm/CRUDDemoForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDDemoForm
+{
+    public partial class DemoForm : Form
+    {
+        public DemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-JPGVJHJI\\SQLEXPRESS;Initial Catalog=CRUDDemoForm;Integrated Security=True;");
+            con.Open();
+            string query = "SET IDENTITY_INSERT tbl_CRUD ON; INSERT INTO tbl_CRUD(ID , Name , Age) Values (@id , @name , @age); SET IDENTITY_INSERT tbl_CRUD OFF";
+            SqlCommand cmd = new SqlCommand(query , con);
+            cmd.Parameters.AddWithValue("@id" , int.Parse(TxtId.Text));
+            cmd.Parameters.AddWithValue("@name", TxtName.Text);
+            cmd.Parameters.AddWithValue("@age" , double.Parse(TxtAge.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully saved");
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP-JPGVJHJI\\SQLEXPRESS; Initial Catalog=CRUDDemoForm; Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update tbl_CRUD set name = @name , age = @age where id = @id" , con);
+            cmd.Parameters.AddWithValue("@id" , int.Parse(TxtId.Text));
+            cmd.Parameters.AddWithValue("@name", TxtName.Text);
+            cmd.Parameters.AddWithValue("@age" , double.Parse (TxtAge.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Updated");
+
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data Source=LAPTOP-JPGVJHJI\\SQLEXPRESS; Initial Catalog=CRUDDemoForm; Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete tbl_CRUD where id = @id" , con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(TxtId.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully deleted");
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data Source = LAPTOP-JPGVJHJI\\SQLEXPRESS; Initial Catalog=CRUDDemoForm; Integrated Security = True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tbl_CRUD", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            DataGridView.DataSource = dt;
+        }
+    }
+}
