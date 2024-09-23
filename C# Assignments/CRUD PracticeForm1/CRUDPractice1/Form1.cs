0db719d7a85559d913a6b1cf0bcb344fbdeef03a
Krutika Chauhan <krutikachauhan07@gmail.com>
2024-09-23 17:41:28 +0530

    Adding Form Validations to the previous code

--- C# Assignments/CRUD PracticeForm1/CRUDPractice1/Form1.cs
+++ C# Assignments/CRUD PracticeForm1/CRUDPractice1/Form1.cs
@@ -10,13 +10,46 @@ namespace CRUDPractice1
         public PracticeForm1()
         {
             InitializeComponent();
+            errorProvider1 = new ErrorProvider();
         }
         SqlConnection con = new SqlConnection("Data Source=LAPTOP-JPGVJHJI\\SQLEXPRESS;Initial Catalog=CRUDDemoForm;Integrated Security=True;");
+        private ErrorProvider errorProvider1;
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(txtItemName.Text) ||
+                string.IsNullOrEmpty(txtDesign.Text)  ||
+                string.IsNullOrEmpty(txtProductId.Text) ||
+                CmbColor.SelectedItem == null ||
+                DateTimePicker.Value < DateTime.Today
+                )
+            {
+                MessageBox.Show("Please fill all the required fields. ");
+                return;
+            }
+
+            int productId;
+            if (!int.TryParse(txtProductId.Text, out productId) || productId <= 0)
+            {
+                MessageBox.Show("Invalid Product ID. Please enter a positive integer.");
+                return;
+            }
+
+            if (txtItemName.Text.Length > 50)
+            {
+                MessageBox.Show("Item Name cannot Exceed 50 characters.");
+                return;
+            }
+
+            if (txtDesign.Text.Length > 50)
+            {
+                MessageBox.Show("Design cannot exceed 50 characters.");
+                return;
+            }
+
             try
             {
+                // Insertion of Data
                 con.Open();
                 SqlCommand command = new SqlCommand("Insert into tbl_PracticeForm1 (ProductId, ItemName, Design, Colour, UpdateDate) values (@ProductId, @ItemName, @Design, @Colour, @UpdateDate)", con);
                 command.Parameters.AddWithValue("@ProductId", int.Parse(txtProductId.Text));
@@ -48,10 +81,28 @@ namespace CRUDPractice1
             Binddata();
         }
 
+        // Update Operation
         private void button2_Click(object sender, EventArgs e)
         {
+
+            if (string.IsNullOrEmpty(txtItemName.Text) ||
+                string.IsNullOrEmpty(txtDesign.Text) ||
+                string.IsNullOrEmpty(txtProductId.Text) ||
+                CmbColor.SelectedItem == null ||
+                DateTimePicker.Value < DateTime.Today)
+            {
+                MessageBox.Show("Please fill all the required fields.");
+                return;
+            }
+            int productId;
+            if (!int.TryParse(txtProductId.Text, out productId) || productId <= 0)
+            {
+                MessageBox.Show("Invalid Product ID. Please enter a positive integer.");
+                return;
+            }
+
             con.Open();
-            SqlCommand command = new SqlCommand("Update tbl_PracticeForm1 set ItemName = @ItemName, Design = @Design, Colour = @Colour, UpdateDate = '"+DateTime.Parse(DateTimePicker.Text)+"' where ProductID = @ProductId", con);
+            SqlCommand command = new SqlCommand("Update tbl_PracticeForm1 set ItemName = @ItemName, Design = @Design, Colour = @Colour, UpdateDate = '" + DateTime.Parse(DateTimePicker.Text) + "' where ProductID = @ProductId", con);
             command.Parameters.AddWithValue("@ProductId", int.Parse(txtProductId.Text));
             command.Parameters.AddWithValue("@ItemName", txtItemName.Text);
             command.Parameters.AddWithValue("@Design", txtDesign.Text);
@@ -90,5 +141,31 @@ namespace CRUDPractice1
             da.Fill(dt);
             dataGridView1.DataSource = dt;
         }
+
+        private void DateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            if (DateTimePicker.Value < DateTime.Today)
+            {
+                errorProvider1.SetError(DateTimePicker, "Update date cannot be in the past");
+            }
+            else
+            {
+                errorProvider1.SetError(DateTimePicker, string.Empty);
+            }
+        }
+
+        private void txtProductId_TextChanged(object sender, EventArgs e)
+        {
+            int productId;
+            if (!int.TryParse(txtProductId.Text, out productId))
+            {
+                errorProvider1.SetError(txtProductId, "Invalid Product Id");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
     }
 }
