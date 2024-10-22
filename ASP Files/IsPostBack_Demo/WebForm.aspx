5c68a301f2294d2203457de5e99166b4153bcc7c
Krutika Chauhan <krutikachauhan07@gmail.com>
2024-10-22 23:00:24 +0530

    Adding ASP web forms

---
+++ ASP Files/IsPostBack_Demo/WebForm.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="IsPostBackproperty.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server" Height="173px" Width="286px"></asp:ListBox>
+
+        </div>
+            <asp:Button ID="Button1" runat="server" Text="CLICK" Font-Bold="True" />
+    </form>
+</body>
+</html>
---
+++ ASP Files/IsPostBack_Demo/WebForm.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/*
+1.PostBack is actually sending all the information from client to the web server , 
+then web server process all those contents and returns back to the client.
+2. When a page loads for the very first time then the IsPostBack Property is False.
+3. When a page loads for the Second Time or more than second time then IsPostBack Property is True.
+4. Whenever we click the button page loads and PostBack occurs.
+*/
+
+namespace IsPostBackproperty
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if(!IsPostBack)
+            //  {
+            //      Response.Write("THIS IS POST BACK REQUEST !!");
+            //  }
+
+            if (!IsPostBack)
+            {
+                AddListBoxItems();
+            }
+        }
+
+        void AddListBoxItems()
+        {
+            ListBox1.Items.Add("Item 1");
+            ListBox1.Items.Add("Item 2");
+            ListBox1.Items.Add("Item 3");
+        }
+    }
+}
