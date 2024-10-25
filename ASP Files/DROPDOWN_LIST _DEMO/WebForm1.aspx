204d5a7d4a1575facdb33ee8abe2739e9f26c63f
Astro7-9-2000 <krutikachauhan07@gmail.com>
2024-10-25 14:12:22 +0530

    Adding DropDown List Programs in ASP

---
+++ ASP Files/DROPDOWN_LIST _DEMO/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<!-- 1. <%@ Page ... %>: This is a page directive that provides information about the ASP.NET page.
+     2. Language="C#": Specifies that the code-behind for this page is written in C#  
+     3. AutoEventWireup="true": This allows ASP.NET to automatically wire up event handlers for events like Page_Load without needing to explicitly declare them in the code-behind file.
+     4. CodeBehind="WebForm1.aspx.cs": Specifies the file that contains the code-behind logic for this ASP.NET page.
+     5. Inherits="DropDownList.WebForm1": Indicates the fully qualified name of the class that this page inherits from, which is WebForm1 in the DropDownList namespace.-->
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropDownList.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server" Height="39px" Width="256px">
+            <asp:ListItem Value ="-1" Text = "select City"></asp:ListItem>
+            <asp:ListItem Value="1" Selected = "True">Pune</asp:ListItem>
+            <asp:ListItem Value="2" Text ="Chennai"></asp:ListItem>
+            <asp:ListItem Value="3">Mumbai</asp:ListItem>
+            <asp:ListItem Value="4" Enabled = "false">Hyderabad</asp:ListItem>
+        </asp:DropDownList>
+        <div>
+            <asp:Button ID="Submit_Btn" runat="server" Font-Bold="True" Text="Submit" OnClick="Submit_Btn_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP Files/DROPDOWN_LIST _DEMO/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.SelectedIndex = 1;
+            
+                //ListItem l1 = new ListItem("Kanpur", "1");
+                //ListItem l2 = new ListItem("Raipur", "2");
+                //ListItem l3 = new ListItem("Nagpur", "3");
+                // false property hides the Solapur item in the dropdown.
+                //ListItem l4 = new ListItem("Solapur", "4", false);
+
+                //DropDownList1.Items.Add(l1);
+                //DropDownList1.Items.Add(l2);
+                //DropDownList1.Items.Add(l3);
+                //DropDownList1.Items.Add(l4);
+
+                // Automatically Pre-selects the Property in the dropdown index 1 when page loads. 
+                //DropDownList1.Items[1].Selected = true;
+            }
+        }
+
+        protected void Submit_Btn_Click(object sender, EventArgs e)
+        {
+           if(DropDownList1.SelectedValue == "-1")
+            {
+                Response.Write("Please select a city");
+            }
+            else
+            {
+                /* This code checks the selected value of DropDownList1.
+                If the selected value is "-1" (indicating that no valid city is selected), it prompts the user to select a city.
+                If a valid city is selected, it outputs the selected item's text, value, and index to the response. 
+                This information is displayed on the web page.*/
+
+                Response.Write("Selected Item Text is: " + DropDownList1.SelectedItem.Text + "<br>");
+                Response.Write("Selected Item Value is: " + DropDownList1.SelectedItem.Value + "<br>");
+                Response.Write("Selected Item Index is: " + DropDownList1.SelectedIndex + "<br>");
+            }
+        }
+    }
+}
