54085b878b2e93d0a737e2285ca192955287370b
Astro7-9-2000 <krutikachauhan07@gmail.com>
2024-12-26 16:44:30 +0530

    Adding files for View Demonstation in MVC

---
+++ MVC/MVCViewDemo/MVCViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string MyController()
+        {
+            return "Hi , I am your Controller";
+        }
+
+        public ActionResult MyView()
+        {
+            return View();
+        }
+    }
+}
